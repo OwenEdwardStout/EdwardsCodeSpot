7709b575fd8b9427a0fb69df080b4f713bd84902
OwenEdwardStout <ostout61@ladueschools.net>
2022-12-16 18:12:22 +0000

    halba

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main (String[]args){
+        int result;
         Questions q = new Questions();
         FortuneTeller x = new FortuneTeller();
         int introEnding = x.introScript1();
         if(introEnding==1){
-            System.out.println("You got " +q.quiz());
-        } else if (introEnding==1){
+result = q.quiz();
+            
+        } else if (introEnding==2){
             System.out.println("Leave then.");
         }
         
--- Questions.java
+++ Questions.java
@@ -15,12 +15,31 @@ public class Questions {
         answer = false;
         acceptedInput=true;
     } else{
-        System.out.println("WHAT ABOUT YES OR NO DIDN'T YOU UNDERSTAND");
+        System.out.print("WHAT ABOUT YES OR NO DIDN'T YOU UNDERSTAND ");
 
     }
 }
 return answer;
+}
+public void questionsP2(int x){
+    int fortune = (int) (Math.random()*3);
+    if(x==0){
+        System.out.println("You didn't get a single question right... That's... huh. Well let's take a look at your fortune.");
+        if (fortune==0){
+            System.out.println("In the years to come you'll find yourself down on your luck. Broke. Alone. But there will be a light at the end of the tunnel. It's a train. Things don't look good.");
+        } else if (fortune==1){
+            System.out.println("Things are looking up! Right up stink creek that is. You seem to be on the road to loosing your house... uh oh...");
+        } else if (fortune==2){
+            System.out.println("Uh oh. I don't see much. How good is your life insurance? I'd make sure you get it up to snuff soon.");
+        }
+    } else if(x==1){
+
+    } else if(x==2){
+
+    } else if(x==3){
+
     }
+}
     public int quiz(){
         int correctTotal = 0;
         for(int x = 0; x < 3; x++){
